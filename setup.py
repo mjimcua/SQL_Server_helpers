19abfb3a8de9efb3f5c136b307788fa4a3a80048
Miguel Ángel Jiménez Cuadrillero <106736040+miguelangeljimenezcuadrillero@users.noreply.github.com>
2022-08-09 13:52:22 +0200

    First version without test

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['sqlalchemy','urllib'],
+    install_requires=['sqlalchemy'],
     python_requires='~=3.6'
 )
